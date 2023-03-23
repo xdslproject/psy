4d4d2ce2fb12938009f079c4e7956012172c5f11
Nick Brown <n.brown@epcc.ed.ac.uk>
2023-03-23 15:54:24 +0000

    Transformation to extract the stencil from the FIR and set this up as a bridge between them

---
+++ psy/extract_stencil.py
@@ -0,0 +1,160 @@
+from abc import ABC
+from typing import TypeVar, cast
+from dataclasses import dataclass
+from ftn.dialects import fir
+from xdsl.utils.hints import isa
+from xdsl.dialects.memref import MemRefType
+from xdsl.ir import Operation, SSAValue, OpResult, Attribute, MLContext, Block, Region
+
+from xdsl.pattern_rewriter import (RewritePattern, PatternRewriter,
+                                   op_type_rewrite_pattern,
+                                   PatternRewriteWalker,
+                                   GreedyRewritePatternApplier)
+from xdsl.dialects import builtin, func, llvm
+from xdsl.dialects.experimental import stencil
+
+
+@dataclass
+class _StencilExtractorRewriteBase(RewritePattern, ABC):
+  bridgedFunctions: dict[str, Operation]
+  bridge_id: int
+
+  def __init__(self):
+    self.bridgedFunctions={}
+    self.bridge_id=0
+
+  def get_nested_type(self, in_type, search_type):
+    if isinstance(in_type, search_type): return in_type
+    return self.get_nested_type(in_type.type, search_type)
+    
+  @staticmethod
+  def wrap_stencil_in_func(name, input_types, operations):
+    """
+    Wraps the extracted stencil operations for 
+    """    
+
+    block = Block.from_arg_types(input_types)
+    block.add_ops(operations)
+    body=Region()
+    body.add_block(block)
+    return func.FuncOp.from_region(name, input_types, [], body)
+
+class ExtractStencilOps(_StencilExtractorRewriteBase):
+    """
+    This will extract the stencil operations and replace them with a function call, converting the
+    input array into fir.LLVMPointer type. Note currently this is very limited in terms of assuming
+    one input only and a statically allocated array - it will get more complex as we add support
+    for other things around this.
+    
+    Note that the logic here is fairly simple, we assume all the stencil operations are between the external
+    load and the external store. Hence we grab all of those as a chunk and move them out
+    """
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: stencil.ExternalLoadOp, rewriter: PatternRewriter, /):
+        parent_op=op.parent
+        idx = parent_op.ops.index(op)
+        stencil_ops=[]
+        for i in range(idx, len(op.parent.ops)):
+          stencil_ops.append(op.parent.ops[i])
+          if isinstance(op.parent.ops[i], stencil.ExternalStoreOp):
+            break
+        nt=self.get_nested_type(op.field.typ, fir.ArrayType)
+        ptr_type=fir.LLVMPointerType([nt.type])
+        convert_array=fir.Convert.create(operands=[op.field],
+                    result_types=[ptr_type])
+        function_name="_InternalBridgeStencil_"+str(self.bridge_id)
+
+        call_stencil=fir.Call.create(attributes={"callee": builtin.SymbolRefAttr(function_name)}, operands=[convert_array.results[0]], result_types=[])
+        parent_op.insert_op([convert_array, call_stencil], idx+1)
+        for sop in stencil_ops:
+          parent_op.detach_op(sop)
+        stencil_bridge_fn=_StencilExtractorRewriteBase.wrap_stencil_in_func(function_name, [ptr_type], stencil_ops)
+        self.bridgedFunctions[function_name]=stencil_bridge_fn
+
+class AddExternalFuncDefs(RewritePattern):
+    """
+    Will add in function signatures of externally bridged functions so that the Fortran code
+    can call this
+    """
+    bridge_functions = list[str]
+
+    def __init__(self, bridge_functions):
+      self.bridge_functions=bridge_functions
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, module: builtin.ModuleOp,
+                          rewriter: PatternRewriter, /):
+        # collect all func calls to MPI functions
+        funcs_to_emit: dict[str, tuple[list[Attribute],
+                                       list[Attribute]]] = dict()
+
+        def walker(op: Operation):
+            if not isinstance(op, fir.Call):
+                return
+            if op.callee.string_value() not in self.bridge_functions:
+                return
+            funcs_to_emit[op.callee.string_value()] = (
+                [arg.typ for arg in op.args],
+                [res.typ for res in op.results],
+            )
+
+        module.walk(walker)
+
+        # for each func found, add a FuncOp to the top of the module.
+        for name, types in funcs_to_emit.items():
+            arg, res = types
+            rewriter.insert_op_at_pos(func.FuncOp.external(name, arg, res),
+                                      module.body.blocks[0],
+                                      len(module.body.blocks[0].ops))
+
+class ConnectExternalLoadToFunctionInput(RewritePattern):
+  """
+  Connects external load at the start of the bridged function with the 
+  input argument (note we will need to load this into a memref here)
+  """
+  @op_type_rewrite_pattern
+  def match_and_rewrite(self, op: stencil.ExternalLoadOp, rewriter: PatternRewriter, /):  
+    op.operands=[op.parent.args[0]]
+    
+class ConnectExternalStoreToFunctionInput(RewritePattern):
+  """
+  Connects the external store at the end of the bridged function with the
+  input argument (note we will need to load this into a memref and then an llvm pointer)
+  """
+  @op_type_rewrite_pattern
+  def match_and_rewrite(self, op: stencil.ExternalStoreOp, rewriter: PatternRewriter, /):  
+    op.operands=[op.temp, op.parent.args[0]]
+
+def extract_stencil(ctx: MLContext, module: builtin.ModuleOp):
+    # First extract the stencil aspects and replace with a function call
+    extractStencil=ExtractStencilOps()
+    walker1 = PatternRewriteWalker(GreedyRewritePatternApplier([
+              extractStencil,
+    ]),
+                                   apply_recursively=True)
+    walker1.rewrite_module(module)
+
+    # Now add in external function signature for new bridged functions
+    bridged_fn_names=[v for v in extractStencil.bridgedFunctions.keys()]
+    walker2 = PatternRewriteWalker(AddExternalFuncDefs(bridged_fn_names))
+    walker2.rewrite_module(module)    
+
+    # Create a new module with all the stencil functions as part of it
+    bridged_functions=[v for v in extractStencil.bridgedFunctions.values()]        
+    new_module=builtin.ModuleOp.from_region_or_ops(bridged_functions)
+
+    # Rewrite the stencil functions to hook up the function/block arguments
+    walker3 = PatternRewriteWalker(GreedyRewritePatternApplier([
+              ConnectExternalLoadToFunctionInput(),
+              ConnectExternalStoreToFunctionInput()
+    ]))
+    walker3.rewrite_module(new_module)
+
+    # Now we want to have two modules packaged together
+    containing_mod=builtin.ModuleOp.from_region_or_ops([])
+    module.regions[0].move_blocks(containing_mod.regions[0])
+    
+    block = Block()
+    block.add_ops([new_module, containing_mod])
+    module.regions[0].add_block(block)
+
